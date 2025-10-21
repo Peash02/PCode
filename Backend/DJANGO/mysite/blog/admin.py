3fd5916c534ae5fbff716ca16f6e2243eeba2508
Dhaiwik RV <dhaiwikrv@gmail.com>
2025-10-22 00:46:08 +0530

    Insert Django Programs

---
+++ Backend/DJANGO/mysite/blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_created')
+admin.site.register(Post, PostAdmin)
+# Register your models here.
